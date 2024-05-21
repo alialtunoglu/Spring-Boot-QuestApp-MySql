3b7ffcaefe0cc576cc7f66d139def4e66dbb1bc8
Ali Altunoğlu <alialtunoglu02@gmail.com>
2024-05-22 00:29:01 +0300

    Added user endpoints and post endpoints

--- src/main/java/com/project/questapp/entities/User.java
+++ src/main/java/com/project/questapp/entities/User.java
@@ -14,4 +14,24 @@ public class User {
 	Long id;
 	String userName;
 	String password;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
